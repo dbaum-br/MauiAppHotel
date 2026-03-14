90384b78521a3040e839f1981bd590bc8933dfb2
daniel baum <dbaum_br@hotmail.com>
2026-03-14 14:24:24 -0300

    Add recursos ao aplicativo

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -7,11 +7,12 @@ namespace MauiAppHotel
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
 
 		protected override Window CreateWindow(IActivationState? activationState)
 		{
+            // Redimensionar janela principal
             var window = base.CreateWindow(activationState);
             window.Width = 400;
             window.Height = 600;
--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,7 +13,10 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+					fonts.AddFont("Caveat-Bold.ttf", "CaveatBold");
+					fonts.AddFont("Caveat-Medium.ttf", "CaveatMedium");
+					fonts.AddFont("Caveat-Regular.ttf", "CaveatRegular");
+				});
 
 #if DEBUG
     		builder.Logging.AddDebug();
